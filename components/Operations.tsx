511a8deea1bdae3b228bd0d7b39cb8c182d38225
jantmartinez <112981816+jantmartinez@users.noreply.github.com>
2025-09-22 00:04:45 +0200

    Updated Operations.tsx

--- components/Operations.tsx
+++ components/Operations.tsx
@@ -518,7 +518,7 @@ const Operations: React.FC<OperationsProps> = ({ trades, accounts, addTrade, clo
                     </>
                   )}
                     {showDeleteButton && (
-                      <>
+                    <>
                     {trade.status === TradeStatus.CLOSED && (
                       <button onClick={() => setTradeToView(trade)} className="font-semibold py-1 px-3 rounded-lg transition-colors text-xs bg-blue-500/20 text-brand-blue hover:bg-blue-500/40">
                         Details
@@ -531,7 +531,7 @@ const Operations: React.FC<OperationsProps> = ({ trades, accounts, addTrade, clo
                       >
                         <i className="ri-delete-bin-line text-base"></i>
                       </button>
-                      </>
+                    </>
                     )}
                   </div>
                 </td>
